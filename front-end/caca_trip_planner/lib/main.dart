fe38f5860c04ce75230fef3fcbff21a6873478a7
huyang <michael531@bupt.edu.cn>
2021-10-12 15:48:01 +0800

    [add] signup screen and Android build config

--- front-end/caca_trip_planner/lib/main.dart
+++ front-end/caca_trip_planner/lib/main.dart
@@ -1,182 +1,40 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:logintunisia/screens/login_via_username_screen.dart';
+import 'package:logintunisia/screens/singup_screen.dart';
 
-const url = 'http://127.0.0.1:8000';
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _State createState() => _State();
-}
-
-class _State extends State<MyApp> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  _createAlertDialog(BuildContext context, String caption, Widget content) {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(caption),
-            content: content,
-            actions: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('OK'),
-              )
-            ],
-          );
-        });
-  }
-
-  void _login(BuildContext context) {
-    http.post(
-      Uri.parse(url + '/auth/login/registered'),
-      body: json.encode({
-        'username': nameController.text,
-        'password': passwordController.text,
-      }),
-      headers: {
-        "Accept": "application/json",
-        "content-type": "application/json"
-      },
-    ).then((response) {
-      var body = json.decode(response.body);
-      // print(body);
-      if (response.statusCode == 200) {
-        // response.body {
-        // }
-        _createAlertDialog(
-            context,
-            '登录成功！',
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('token: ' + body['token'] + '\n'),
-                Text('username: ' + body['username'])
-              ],
-            ));
-      } else {
-        _createAlertDialog(
-            // TODO: handle exception
-            context,
-            '登录失败！',
-            Text('Error: ' + body["non_field_errors"][0]));
-      }
-    });
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('密码登录'),
-        ),
-        body: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: <Widget>[
-                // const SizedBox(
-                //   height: 40,
-                // ),
-                Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.fromLTRB(0, 40, 0, 20),
-                    child: const Text(
-                      '卡卡随心游',
-                      style:
-                          TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
-                    )),
-                // const SizedBox(height: 40,)
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: '用户名/手机号',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: '密码',
-                    ),
-                  ),
-                ),
-                Container(
-                  // padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                  // alignment: Alignment.bottomRight,
-                  child: Row(
-                    children: [
-                      TextButton(
-                        onPressed: () {},
-                        child: const Text('使用其它方式登录'),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          //forgot password screen
-                        },
-                        // textColor: Colors.blue,
-                        child: const Text('忘记密码'),
-                      ),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  ),
-                  // height: 50,
-                ),
-                Container(
-                    width: double.infinity,
-                    height: 60,
-                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                    child: ElevatedButton(
-                      // textColor: Colors.white,
-                      // color: Colors.blue,
-                      child: const Text(
-                        '登录',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
-                      ),
-                      onPressed: () => _login(context),
-                    )),
-                Row(
-                  children: <Widget>[
-                    const Text('还没有卡卡账户？'),
-                    SizedBox(
-                      width: 48,
-                      child: TextButton(
-                        // textColor: Colors.blue,
-                        child: const Text(
-                          '注册',
-                          // style: TextStyle(fontSize: 20),
-                        ),
-                        onPressed: () {
-                          //signup screen
-                        },
-                      ),
-                    )
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.center,
-                )
-              ],
-            )));
+    return MaterialApp(
+      title: '卡卡随心游',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(
+              accentColor: Colors.amber,
+              primarySwatch: Colors.pink,
+              backgroundColor: const Color.fromRGBO(255, 254, 229, 1)),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              caption: const TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ))),
+      initialRoute: LoginViaUsernameScreen.routeName,
+      routes: {
+        LoginViaUsernameScreen.routeName: (ctx) =>
+            const LoginViaUsernameScreen(),
+        SignupScreen.routeName: (ctx) => const SignupScreen(),
+      },
+    );
   }
 }
-
-// test github 2
