035f68de0e73b6f50cda71c5f34398d2adf3025d
huyang <michael531@bupt.edu.cn>
2021-12-08 13:24:42 +0800

    backup

--- front-end/caca_trip_planner/lib/screens/singup_screen.dart
+++ front-end/caca_trip_planner/lib/screens/singup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:debounce_throttle/debounce_throttle.dart';
 
 import '../utils.dart';
 
@@ -21,95 +20,70 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-  TextEditingController portController = TextEditingController();
-  TextEditingController phoneController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+
+  double rw = 0;
+  double rh = 0;
 
   bool _isLoading = false;
   bool _isChecked = false;
-  bool _isValidInfo = false; // 不用get是因为_isValidInfo在一次build中多次被访问，尽量减少开销。
-  bool _hasDuplicateUsername = false;
-  bool get _isLegalPhoneNumber => _checkPhoneNumber();
-  bool get _disableButton => _isLoading == true || _isValidInfo == false;
-  final nameDebouncer = Debouncer<String>(
-    const Duration(milliseconds: 100),
-    initialValue: '',
-  );
+  bool get _isValidInfo =>
+      (_form.currentState?.validate() ?? false) && _isChecked;
+  bool get _disableButton => _isLoading == true || _isChecked == false;
+  final _nameDebouncer = Debouncer(milliseconds: 100);
+  final _values = {
+    'port': '',
+    'username': '',
+    'password': '',
+    'phoneNumber': '',
+  };
 
   @override
   void dispose() {
-    nameController.dispose();
-    passwordController.dispose();
-    portController.dispose();
-    phoneController.dispose();
     super.dispose();
   }
 
   @override
   void initState() {
     super.initState();
-
-    nameController.addListener(() {
-      nameDebouncer.value = nameController.text;
-    });
-    nameDebouncer.values.listen((username) {
-      _checkDuplicateUsername(); // 该函数中间接调用了setState()
-    });
-    portController.addListener(_checkInfo);
-    phoneController.addListener(_checkInfo);
-    passwordController.addListener(_checkInfo);
   }
 
-  void _checkInfo() {
-    setState(() {
-      _isValidInfo = portController.text.isNotEmpty &&
-          nameController.text.isNotEmpty &&
-          !_hasDuplicateUsername &&
-          _isLegalPhoneNumber &&
-          passwordController.text.isNotEmpty &&
-          _isChecked;
-    });
-  }
-
-  void _checkDuplicateUsername() {
-    http
-        .get(Uri.parse(
-            '$url${portController.text}/auth/check/username?username=${nameDebouncer.value}'))
-        .then((response) {
-      if (response.statusCode == 200) {
-        _hasDuplicateUsername = json.decode(response.body)['result'];
-      } else {
-        _hasDuplicateUsername = false;
-      }
-      _checkInfo();
-    });
-  }
-
-  bool _checkPhoneNumber() {
-    return phoneController.text.isEmpty ||
-        (phoneController.text.isNotEmpty && phoneController.text.length == 11);
+  @override
+  void didChangeDependencies() {
+    rh = MediaQuery.of(context).size.height / Utils.h13pm;
+    rw = MediaQuery.of(context).size.width / Utils.w13pm;
+    super.didChangeDependencies();
   }
 
   void _signup(BuildContext ctx) {
+    if (_form.currentState!.validate() == false) {
+      Utils.showMaterialAlertDialog(
+        ctx,
+        '登录失败',
+        const Text('输入的信息不合法'),
+      );
+      return;
+    }
+
+    _form.currentState?.save();
+
     setState(() {
       _isLoading = true;
     });
 
     http
         .post(
-          Uri.parse(url + portController.text + '/auth/register'),
-          body: json.encode({
-            'username': nameController.text,
-            if (phoneController.text.isNotEmpty)
-              'phoneNumber': phoneController.text,
-            'password': passwordController.text,
-          }),
-          headers: jsonHeaders,
-        )
-        .timeout(const Duration(seconds: 3),
-            onTimeout: () => Utils.showMaterialAlertDialog(
-                ctx, '注册失败', const Text('请求超时' '\n\n请检查端口号!')))
+      Uri.parse(url + _values['port']! + '/auth/register'),
+      body: json.encode({
+        'username': _values['username'],
+        if (_values['phone']!.isNotEmpty) 'phoneNumber': _values['phone'],
+        'password': _values['password'],
+      }),
+      headers: jsonHeaders,
+    )
+        // .timeout(const Duration(seconds: 3),
+        // onTimeout: () => Utils.showMaterialAlertDialog(
+        //     ctx, '注册失败', const Text('请求超时' '\n\n请检查端口号!')))
         .catchError((error) {
       Utils.showMaterialAlertDialog(
           ctx, '注册失败', Text(error.toString() + '\n\n请检查端口号!'));
@@ -172,131 +146,177 @@ class _SignupScreenState extends State<SignupScreen> {
           title: const Text('注册'),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            physics: const NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              Container(
-                  alignment: Alignment.center,
-                  padding: const EdgeInsets.fromLTRB(0, 40, 0, 20),
-                  child: const Text(
-                    '卡卡随心游LOGO',
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
-                  )),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
+          padding: EdgeInsets.symmetric(horizontal: 16 * rw, vertical: 16 * rh),
+          child: Form(
+            key: _form,
+            child: ListView(
+              physics: const NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.fromLTRB(0, 40 * rh, 0, 20 * rh),
+                    child: const Text(
+                      '卡卡随心游LOGO',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
+                    )),
+                TextFormField(
                   keyboardType: const TextInputType.numberWithOptions(
                       signed: false, decimal: false),
-                  controller: portController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  textInputAction: TextInputAction.next,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(10 * rh),
                     ),
                     labelText: '端口号',
                   ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return '端口号不能为空！';
+                    }
+                    if (!(value?.isValidPort() ?? false)) {
+                      return '端口号应为1-4位数字';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _values['port'] = value!,
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: nameController,
+                SizedBox(height: 20 * rh),
+                TextFormField(
+                  textInputAction: TextInputAction.next,
                   decoration: InputDecoration(
+                    labelText: '用户名',
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(10 * rh),
                     ),
-                    labelText: _hasDuplicateUsername ? '用户名 - 用户名已存在！' : '用户名',
-                    floatingLabelStyle: _hasDuplicateUsername
-                        ? TextStyle(color: Theme.of(context).errorColor)
-                        : null,
                   ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    _nameDebouncer.run(
+                      () => {
+                        http
+                            .get(Uri.parse(
+                                '$url${_values['port']}/auth/check/username?username=$value'))
+                            .then(
+                          (response) {
+                            if (response.statusCode == 200) {
+                              if (json.decode(response.body)['result'] ==
+                                  true) {
+                                return '用户名已存在！';
+                              }
+                            } else {
+                              return null;
+                            }
+                          },
+                        )
+                      },
+                    );
+                  },
+                  onSaved: (value) => _values['username'] = value!,
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
+                SizedBox(height: 20 * rh),
+                TextFormField(
+                  textInputAction: TextInputAction.next,
                   keyboardType: const TextInputType.numberWithOptions(
                       decimal: false, signed: false),
-                  controller: phoneController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(10 * rh),
                     ),
-                    labelText:
-                        _isLegalPhoneNumber ? '手机号（可选）' : '手机号（可选）- 手机号不合法',
-                    floatingLabelStyle: !_isLegalPhoneNumber
-                        ? TextStyle(color: Theme.of(context).errorColor)
-                        : null,
+                    labelText: '手机号（可选）',
+                    // floatingLabelStyle: !_isLegalPhoneNumber
+                    //     ? TextStyle(color: Theme.of(context).errorColor)
+                    //     : null,
                   ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (!(value?.isValidPhoneNumber() ?? false)) {
+                      return '手机号不合法';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _values['phone'] = value!,
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                child: TextField(
+                SizedBox(height: 20 * rh),
+                TextFormField(
+                  textInputAction: TextInputAction.next,
                   obscureText: true,
-                  controller: passwordController,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
+                      borderRadius: BorderRadius.circular(10 * rh),
                     ),
                     labelText: '密码',
                   ),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return '密码不能为空！';
+                    }
+                    if (!(value?.isValidPassword() ?? false)) {
+                      return '密码至少为8位';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _values['password'] = value!,
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 30,
-                    width: 25,
-                    child: Checkbox(
-                      onChanged: (val) {
-                        _isChecked = val ?? false;
-                        _checkInfo();
-                      },
-                      activeColor: Theme.of(context).colorScheme.primary,
-                      value: _isChecked,
-                      shape: const CircleBorder(),
+                SizedBox(height: 10 * rh),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 30 * rh,
+                      width: 25 * rw,
+                      child: Checkbox(
+                        onChanged: (val) {
+                          setState(() {
+                            _isChecked = val ?? false;
+                          });
+                        },
+                        activeColor: Theme.of(context).colorScheme.primary,
+                        value: _isChecked,
+                        shape: const CircleBorder(),
+                      ),
                     ),
-                  ),
-                  const Text('我已阅读且同意'),
-                  GestureDetector(
-                    child: const Text(
-                      '《用户协议》',
-                      style: TextStyle(color: Colors.blue),
+                    const Text('我已阅读且同意'),
+                    GestureDetector(
+                      child: const Text(
+                        '《用户协议》',
+                        style: TextStyle(color: Colors.blue),
+                      ),
+                      onTap: () => Utils.showMaterialAlertDialog(
+                        context,
+                        '用户协议',
+                        const Text('示例用户协议'),
+                      ),
+                    )
+                  ],
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 60,
+                  padding: EdgeInsets.fromLTRB(10 * rw, 10 * rh, 10 * rw, 0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10 * rh),
+                      )),
                     ),
-                    onTap: () => Utils.showMaterialAlertDialog(
-                      context,
-                      '用户协议',
-                      const Text('示例用户协议'),
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                width: double.infinity,
-                height: 60,
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )),
-                  ),
-                  child: _isLoading
-                      ? const CircularProgressIndicator.adaptive()
-                      : const Text(
-                          '注册',
-                          style: TextStyle(
-                            fontSize: 18,
+                    child: _isLoading
+                        ? const CircularProgressIndicator.adaptive()
+                        : const Text(
+                            '注册',
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
                           ),
-                        ),
-                  // The button would be disabled if onPress is null.
-                  onPressed: _disableButton ? null : () => _signup(context),
+                    // The button would be disabled if onPress is null.
+                    onPressed: _disableButton ? null : () => _signup(context),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- front-end/caca_trip_planner/lib/utils.dart
+++ front-end/caca_trip_planner/lib/utils.dart
@@ -1,25 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'dart:async';
 
 class Utils {
-  static dynamic showMaterialAlertDialog(
+  static const double h13pm = 926.0;
+  static const double w13pm = 428.0;
+
+  static Future<Object?> showMaterialAlertDialog(
       BuildContext ctx, String caption, Widget content) {
     return showDialog(
-        context: ctx,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(caption),
-            content: SingleChildScrollView(
-              child: content,
-            ),
-            actions: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('OK'),
-              )
-            ],
-          );
-        });
+      context: ctx,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(caption),
+          content: SingleChildScrollView(
+            child: content,
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+extension Validator on String {
+  bool isValidPort() {
+    return RegExp(r'^[0-9]{1,4}$').hasMatch(this);
+  }
+
+  bool isValidEmail() {
+    return RegExp(
+            r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+        .hasMatch(this);
+  }
+
+  bool isValidPhoneNumber() {
+    return RegExp(r'^1[0-9]{10}$').hasMatch(this);
+  }
+
+  bool isValidPassword() {
+    return length >= 8;
+  }
+}
+
+class Debouncer {
+  final int milliseconds;
+  VoidCallback? action;
+  Timer? _timer;
+  Debouncer({required this.milliseconds});
+  run(VoidCallback action) {
+    if (_timer?.isActive ?? false) {
+      _timer!.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
   }
 }
