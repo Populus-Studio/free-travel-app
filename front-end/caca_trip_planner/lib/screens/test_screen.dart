9fd0c3b11f8b7787b10be1650c6e764f65d4898d
huyang <michael531@bupt.edu.cn>
2022-05-20 19:18:49 +0800

    final :(

--- front-end/caca_trip_planner/lib/screens/test_screen.dart
+++ front-end/caca_trip_planner/lib/screens/test_screen.dart
@@ -23,7 +23,7 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   final TextEditingController _tripIdController = TextEditingController();
 
-  String tripId = '9';
+  String tripId = '62';
 
   @override
   Widget build(BuildContext context) {
